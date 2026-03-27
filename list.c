d5f19e288fa5eaffc35a509fb0f7e9f9c7921263
Your Name <you@example.com>
2026-03-27 00:22:40 +0000

    new try

--- list.c
+++ list.c
@@ -49,7 +49,7 @@ List * createList() {
 
 void * firstList(List * list) {
 
-    if (list == NULL) return NULL;
+    //if (list == NULL) return NULL;
     if (list -> head == NULL) return NULL;
 
     list -> current = list -> head;
@@ -75,11 +75,24 @@ void * nextList(List * list) {
 //   - La segunda función retorna el dato del nodo anterior a current y actualiza el current para que apunte a ese nodo.
 
 void * lastList(List * list) {
-    return NULL;
+
+    if (list -> tail == NULL) return NULL;
+
+    list -> current = list -> tail;
+
+    return list -> current -> data;
+
 }
 
 void * prevList(List * list) {
-    return NULL;
+
+    if (list -> current == NULL) return NULL;
+    if (list -> current -> prev == NULL) return NULL;
+
+    list -> current = list -> current -> prev;
+
+    return list -> current -> data;
+    
 }
 
 // 4. Programe la función void pushFront(List * list, void * data), la cual agrega un dato al comienzo de la lista.
